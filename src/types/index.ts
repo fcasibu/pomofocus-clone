37d471dfccfbe740463266f87361d09f93e9eb7b
nevz <projectnevz@gmail.com>
2023-03-13 00:30:43 +0800

    feat: add FlattenKeys type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,10 @@
 import type { TIMER_NAME } from '../constants';
 
 export type TimerName = keyof typeof TIMER_NAME;
+
+// Credits to: https://www.raygesualdo.com/posts/flattening-object-keys-with-typescript-types
+export type FlattenKeys<T extends Record<string, unknown>, K = keyof T> = K extends string
+  ? T[K] extends Record<string, unknown>
+    ? `${K}.${FlattenKeys<T[K]>}`
+    : `${K}`
+  : never;
