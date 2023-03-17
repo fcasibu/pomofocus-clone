a4028b292f0f24ebd15b8b3e26735831c0cce2f2
fcasibu <fcasibu@users.noreply.github.com>
2023-03-17 05:44:36 +0000

    deploy: 3f2ac1d25ef3e22dd108c03f182647ec1c944b97

---
+++ assets/App-09e954e7.js
@@ -0,0 +1,14 @@
+import{s as a,a as t,u as d,b as c,c as l,j as e,F as u,d as g,e as s,H as m,T as h,M as p,S as x}from"./index-9fb7d052.js";const o={Background:a.div`
+    background-color: ${({theme:n})=>n.bg};
+    height: 100%;
+    transition: background-color 0.5s;
+
+    @media (prefers-reduced-motion) {
+      transition: none;
+    }
+  `,Container:a.div`
+    height: 100%;
+    margin: 0 auto;
+    max-width: 640px;
+    padding: 14px ${t.XXXS};
+  `};function b(){const{currentTimerName:n}=d(),{openedModal:r}=c(),{theme:i}=l();return e(u,{theme:{bg:i.colorThemes[n],text:g.WHITE},children:e(o.Background,{children:s(o.Container,{children:[e(m,{}),s("main",{children:[e(h,{}),r==="settings"?e(p,{children:e(x,{})}):null]})]})})})}export{b as default};
