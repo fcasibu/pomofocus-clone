1d4dacf5f538ad3ea2de1ebd0ca803b66be0d89f
nevz <projectnevz@gmail.com>
2023-03-17 13:32:53 +0800

    feat: add alarm sounds

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as Alarm_Analog } from './alarm_analog.mp3';
+export { default as Alarm_Bell_1 } from './alarm_bell_1.mp3';
+export { default as Alarm_Bell_2 } from './alarm_bell_2.mp3';
+export { default as ClickSound } from './rclick.mp3';
