54bd9f9c2d8d19ac762964b307de05ec996f1724
nevz <projectnevz@gmail.com>
2023-03-17 13:35:03 +0800

    feat: add Range and Select component

---
+++ src/components/common/Input/Range.tsx
@@ -0,0 +1,92 @@
+import type { Config, ConfigKeys } from '@context';
+import { useConfig } from '@hooks/useConfig';
+import { colors, playAudio, spacing } from '@utils';
+import produce from 'immer';
+import type { ChangeEvent, InputHTMLAttributes } from 'react';
+import { useCallback, useState } from 'react';
+import type { Control } from 'react-hook-form';
+import { useController } from 'react-hook-form';
+import styled from 'styled-components';
+
+export interface RangeProps extends InputHTMLAttributes<HTMLInputElement> {
+  control: Control<Config>;
+  name: ConfigKeys;
+  label?: string;
+  isAudio?: boolean;
+}
+
+const S = {
+  Container: styled.div`
+    align-items: center;
+    display: flex;
+    gap: ${spacing.XXS};
+
+    > span {
+      color: ${colors.LIGHTER_GREY};
+    }
+  `,
+
+  Input: styled.input`
+    appearance: none;
+    -webkit-appearance: none;
+    border-radius: 9999px;
+    height: 6px;
+    width: 100%;
+    background: ${colors.LIGHTER_GREY};
+    cursor: pointer;
+  `,
+};
+
+function debounce(fn: (...args: any[]) => void, ms = 1000) {
+  let timeout: number;
+
+  return (...args: any[]) => {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      fn(...args);
+    }, ms);
+  };
+}
+
+export const Range = ({ label, control, name, isAudio = true, ...props }: RangeProps) => {
+  const { sound, updateSound } = useConfig();
+  const { field } = useController({
+    control,
+    name,
+  });
+  const play = useCallback(debounce(playAudio, 500), []);
+  const [value, setValue] = useState(Math.floor((field.value as number) * 100));
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const v = Number(event.target.value) / 100;
+    setValue(Math.floor(v * 100));
+    field.onChange(v);
+    if (isAudio) {
+      updateSound(
+        produce(sound, (draft) => {
+          draft.alarm.gain = v;
+        }),
+      );
+      play(sound.alarm.sound, v);
+    }
+  };
+
+  return (
+    <S.Container>
+      <span>{value}</span>
+      <S.Input
+        aria-label={`Slide Range for ${label}`}
+        type="range"
+        min={0}
+        max={100}
+        step={1}
+        value={value}
+        onChange={handleChange}
+        {...props}
+      />
+    </S.Container>
+  );
+};
+
+export default Range;
