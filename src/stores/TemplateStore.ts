aa0d41fc980246e51b4367f989cb6973468e0dcd
nevz <projectnevz@gmail.com>
2023-04-17 11:33:06 +0800

    fix: unselect added tasks in template

--- src/stores/TemplateStore.ts
+++ src/stores/TemplateStore.ts
@@ -34,10 +34,12 @@ export const useTemplateStore = create(
     ...initialState,
     addTemplate: (name: string) => {
       const tasksStore = useTasksStore.getState();
-      const tasksToAdd = get().templates[name].map((task, idx) => ({
+      const tasksToAdd = get().templates[name].map((task) => ({
         ...task,
         id: crypto.randomUUID(),
-        isSelected: tasksStore.tasks.length === 0 && idx === 0,
+        isSelected: false,
+        isFinished: false,
+        finishedPomodoros: 0,
       }));
       if (tasksToAdd) {
         tasksStore.addTasks(tasksToAdd);
